fcab760e8e94569d4f973c53621f58b2a6fd0270
Zombie <158185295+ZombieGeeK0@users.noreply.github.com>
2024-01-30 17:49:36 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+clear
+echo --------------------------------------------------------------------------------------------
+echo [~] Instalando requerimientos...
+echo --------------------------------------------------------------------------------------------
+sudo su
+sudo apt-get update
+sudo apt-get install python3
+sudo apt-get install python3-pip
+sudo apt install msfvenom
+pip3 install os
+pip3 install colarama
+pip3 install sys
+pip3 install time
+pip3 install socket
+echo --------------------------------------------------------------------------------------------
+echo [~] Requerimientos instalados. Usa el comando python3 main.py para inciar Scorpion.
